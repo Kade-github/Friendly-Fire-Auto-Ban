6e372986923c33bee16ac8c42b9c48fe7e6a0ff1
KadePcGames <kadepcgames@gmail.com>
2020-01-18 19:57:44 -0800

    Fixed bug

--- FriendlyFireAutoBan-EXILED/Eventhandlers.cs
+++ FriendlyFireAutoBan-EXILED/Eventhandlers.cs
@@ -26,6 +26,8 @@ namespace FriendlyFireAutoBan_EXILED
         {
             if (ev.Attacker == null || ev.Player == null)
                 return;
+            if (!ev.Player.characterClassManager.isClient || !ev.Attacker.characterClassManager.isClient)
+                return;
 
             Team attackerT = Plugin.GetTeam(ev.Attacker.characterClassManager.CurClass);
             Team attackedT = Plugin.GetTeam(ev.Player.characterClassManager.CurClass);
@@ -41,8 +43,16 @@ namespace FriendlyFireAutoBan_EXILED
 
         internal void WaitingForPlayers() => playerTable.Clear();
 
-        internal void OnPlayerJoin(PlayerJoinEvent ev) => playerTable.Add(ev.Player, 0);
+        internal void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            if (ev.Player.characterClassManager.isClient)
+                playerTable.Add(ev.Player, 0);
+        }
 
-        internal void OnPlayerLeave(PlayerLeaveEvent ev) => playerTable.Remove(ev.Player);
+        internal void OnPlayerLeave(PlayerLeaveEvent ev)
+        {
+            if (ev.Player.characterClassManager.isClient)
+                playerTable.Remove(ev.Player);
+        }
     }
 }
