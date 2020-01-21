5167369afc1c1ea570c29dce7ae706bf42db8fca
Kade <KadePcGames@gmail.com>
2020-01-21 00:06:35 -0800

    2.0b

--- FriendlyFireAutoBan-EXILED/FFAB.cs
+++ FriendlyFireAutoBan-EXILED/FFAB.cs
@@ -1,4 +1,4 @@
-﻿using EXILED;
+using EXILED;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,16 +30,19 @@ namespace FriendlyFireAutoBan_EXILED
 
         public override void OnEnable()
         {
+            if (!Config.GetBool("ffab_enable", true))
+                return;
             // e n a b l e
             int ban = Config.GetInt("ffab_banMins", 120);
             int threshhold = Config.GetInt("ffab_damageThreshold", 450);
 
             EventHandlers = new EventHandlers(this, threshhold, ban);
 
-            Events.PlayerHurtEvent += EventHandlers.OnPlayerHurt;
             Events.PlayerJoinEvent += EventHandlers.OnPlayerJoin;
             Events.PlayerLeaveEvent += EventHandlers.OnPlayerLeave;
             Events.WaitingForPlayersEvent += EventHandlers.WaitingForPlayers;
+            Events.PlayerHurtEvent += EventHandlers.OnPlayerHurt;
+
 
             Info("FFAB Enabled! Current Version: " + version);
         }
